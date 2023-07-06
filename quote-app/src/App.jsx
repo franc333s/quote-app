791857a68ad48963f04955cca9822d209fde1972
franc333s <marinafase@gmail.com>
2023-07-06 20:28:13 +0200

    Initial vite setup

---
+++ quote-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './scss/global.scss'
+
+function App() {
+
+  return (
+    <>
+            <img src='/quote-app.png' alt='QuoteApp' />
+            <h1>QuoteApp</h1>
+            <p>This is App.jsx</p>
+    </>
+  )
+}
+
+export default App
